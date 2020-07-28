902f51d876d039aaa46b7e757090a7c82d629273
isakharam <52228419+isakharam@users.noreply.github.com>
2020-07-28 14:56:59 +0530

    Create day15.py
    day 15 : Linked List

---
+++ day15.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+    def insert(self,head,data):
+        if head is None:
+            head=Node(data)
+            return head
+        iteration = head
+        while iteration.next:
+            iteration=iteration.next
+        iteration.next=Node(data)
+        return head
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+mylist.display(head); 	  
